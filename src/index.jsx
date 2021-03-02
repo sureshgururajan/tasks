aa3d6d91571bbe0bf752c0bd30bf397a997cd530
Suresh Gururajan <gusuresh@amazon.com>
2021-03-02 07:58:51 +0530

    Fix auth issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 import App from './App';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
     <React.StrictMode>
